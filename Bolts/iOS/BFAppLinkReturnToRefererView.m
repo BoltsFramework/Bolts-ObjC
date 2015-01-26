8f1bb4a5962c12d3ee94a1d050d3db7589a25488
Joseph Earl <josephearl@powa.com>
2015-01-26 20:01:16 +0000

    Increase size of hit targets

--- Bolts/iOS/BFAppLinkReturnToRefererView.m
+++ Bolts/iOS/BFAppLinkReturnToRefererView.m
@@ -71,6 +71,7 @@ static const CGFloat BFCloseButtonHeight = 12.0;
         _closeButton.userInteractionEnabled = YES;
         _closeButton.clipsToBounds = YES;
         _closeButton.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleTopMargin;
+        _closeButton.contentMode = UIViewContentModeCenter;
         [_closeButton addTarget:self action:@selector(closeButtonTapped:) forControlEvents:UIControlEventTouchUpInside];
 
         [self addSubview:_closeButton];
@@ -108,14 +109,14 @@ static const CGFloat BFCloseButtonHeight = 12.0;
     CGSize labelSize = [_labelView sizeThatFits:bounds.size];
     _labelView.preferredMaxLayoutWidth = _labelView.bounds.size.width;
     _labelView.frame = CGRectMake(BFMarginX,
-                                  CGRectGetMaxY(bounds) - labelSize.height - BFMarginY,
+                                  CGRectGetMaxY(bounds) - labelSize.height - 1.5 * BFMarginY,
                                   CGRectGetMaxX(bounds) - BFCloseButtonWidth - 3 * BFMarginX,
-                                  labelSize.height);
+                                  labelSize.height + BFMarginY);
 
-    _closeButton.frame = CGRectMake(CGRectGetMaxX(bounds) - BFCloseButtonWidth - BFMarginX,
-                                    _labelView.center.y - BFCloseButtonHeight / 2.0,
-                                    BFCloseButtonWidth,
-                                    BFCloseButtonHeight);
+    _closeButton.frame = CGRectMake(CGRectGetMaxX(bounds) - BFCloseButtonWidth - 2 * BFMarginX,
+                                    _labelView.center.y - BFCloseButtonHeight / 2.0 - BFMarginY,
+                                    BFCloseButtonWidth + 2 * BFMarginX,
+                                    BFCloseButtonHeight + 2 * BFMarginY);
 }
 
 - (CGSize)sizeThatFits:(CGSize)size {
@@ -169,7 +170,7 @@ static const CGFloat BFCloseButtonHeight = 12.0;
     UIImage *closeButtonImage = [self drawCloseButtonImageWithColor:_textColor];
 
     _labelView.textColor = _textColor;
-    [_closeButton setBackgroundImage:closeButtonImage forState:UIControlStateNormal];
+    [_closeButton setImage:closeButtonImage forState:UIControlStateNormal];
 }
 
 - (UIImage *)drawCloseButtonImageWithColor:(UIColor *)color {
