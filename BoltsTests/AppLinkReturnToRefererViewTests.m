a51f46fee044da60081bd6605eef26082a2a7d83
Nikita Lutsenko <nlutsenko@fb.com>
2015-08-10 16:08:00 -0700

    Add tests for lines missing coverage.

--- BoltsTests/AppLinkReturnToRefererViewTests.m
+++ BoltsTests/AppLinkReturnToRefererViewTests.m
@@ -23,16 +23,6 @@ static NSString *const BFURLWithRefererNameNoUrl = @"bolts://?foo=bar&al_applink
 
 @implementation AppLinkReturnToRefererViewTests
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here; it will be run once, before the first test case.
-}
-
-- (void)tearDown {
-    // Put teardown code here; it will be run once, after the last test case.
-    [super tearDown];
-}
-
 - (void)testInitReturnsValidView {
     BFAppLinkReturnToRefererView *view = [[BFAppLinkReturnToRefererView alloc] init];
 
