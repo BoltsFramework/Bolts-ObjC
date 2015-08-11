542bc465ea4012bb9f6b8afde3e03f34c268afe6
Nikita Lutsenko <nlutsenko@fb.com>
2015-08-11 13:31:15 -0700

    Add backward compatible Obj-C Generic support for Tasks.

--- Bolts/Common/Bolts.h
+++ Bolts/Common/Bolts.h
@@ -12,6 +12,7 @@
 #import <Bolts/BFCancellationToken.h>
 #import <Bolts/BFCancellationTokenRegistration.h>
 #import <Bolts/BFCancellationTokenSource.h>
+#import <Bolts/BFDefines.h>
 #import <Bolts/BFExecutor.h>
 #import <Bolts/BFTask.h>
 #import <Bolts/BFTaskCompletionSource.h>
