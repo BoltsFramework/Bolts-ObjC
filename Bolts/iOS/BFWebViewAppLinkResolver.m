6ef61c4b0bac1d47e02cd78f3ced607c7fbdcba9
Nikita Lutsenko <nlutsenko@me.com>
2016-02-20 19:54:27 -0800

    Fix warnings when compling with latest Xcode.

--- Bolts/iOS/BFWebViewAppLinkResolver.m
+++ Bolts/iOS/BFWebViewAppLinkResolver.m
@@ -249,6 +249,9 @@ static NSString *const BFWebViewAppLinkResolverShouldFallbackKey = @"should_fall
 #ifdef __TVOS_9_0
         case UIUserInterfaceIdiomTV:
 #endif
+#ifdef __IPHONE_9_3
+        case UIUserInterfaceIdiomCarPlay:
+#endif
         case UIUserInterfaceIdiomUnspecified:
         default:
             // Future-proofing. Other User Interface idioms should only hit ios.
